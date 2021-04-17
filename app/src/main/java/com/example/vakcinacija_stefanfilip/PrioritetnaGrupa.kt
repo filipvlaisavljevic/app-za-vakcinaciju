687f2bf1320b249b5b937a8a2cab3aa121fd6124
Steffe Mech <stefkoRFlee@hotmail.com>
2021-04-18 00:37:43 +0200

    Napravio sam prioritetne grupe. Uradio sam sljedece:
    1. Naslov, text i btn sve dimenzije, boje i pozicije kako smo rekli
    2. Nisam imao bolju ideju pa sam stavio checkbox za ove grupe, mozemo to promjeniti ako nije dobar pristup

---
+++ app/src/main/java/com/example/vakcinacija_stefanfilip/PrioritetnaGrupa.kt
@@ -0,0 +1,11 @@
+package com.example.vakcinacija_stefanfilip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrioritetnaGrupa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.prioritetna_grupa)
+    }
+}
