024473c1235c6226f32c5686e5d7afbd1349827a
Steffe Mech <stefkoRFlee@hotmail.com>
2021-04-18 13:09:31 +0200

    Uradio sam dodatno "Sazetak prethodnih odgovora", ne znam hocu li poslije imati vremena pa reko bolje da nesto jos uradim... Ugl, pogledaj pa ako bude sta trebalo mijenjati javi.

---
+++ app/src/main/java/com/example/vakcinacija_stefanfilip/SazetakPrethodnihOdgovora.kt
@@ -0,0 +1,11 @@
+package com.example.vakcinacija_stefanfilip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SazetakPrethodnihOdgovora : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sazetak_prethodnih_odgovora)
+    }
+}
