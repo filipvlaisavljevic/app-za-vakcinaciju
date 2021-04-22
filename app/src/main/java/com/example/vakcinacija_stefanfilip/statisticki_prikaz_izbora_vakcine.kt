4e703f82fbfa0b43d6d0a2d7fcecc6043b6cdf49
Steffe Mech <stefkoRFlee@hotmail.com>
2021-04-22 19:30:19 +0200

    TEZA ZADATKA: "Također, može se razraditi logika za simulaciju do sada registrovanih korisnika, kao i eventualni statistički prikaz broja izbora svakog proizvođača vakcine, ali ovo nije uslov za osvajanje maksimalnog broja bodova."
    Uradio sam ovaj dio i napravio stupcastu statistiku za vakcine, tj za broj ljudi koji se prijavio za svaku od vakcina. Stavio sam da imamo 4 vakcine i mislio da i ti kad budes pravio ibor vakcine stavis iste ove 4 ... i da tu dodas jos button npr. "pogledaj statistiku" prije odabira vakcine. Kad kliknu to dugme pali se vamo ova statistika i imaju onu strelicu nazad da se vrate na odabir, to je lako kasnije napravit. Isto, na layoutu ti nece prikazati diagram, pise no data al kad pokrenes emulator na tom layoutu prikaze sve

---
+++ app/src/main/java/com/example/vakcinacija_stefanfilip/statisticki_prikaz_izbora_vakcine.kt
@@ -0,0 +1,53 @@
+package com.example.vakcinacija_stefanfilip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+
+class statisticki_prikaz_izbora_vakcine : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statisticki_prikaz_izbora_vakcine);
+        postaviStatistiku();
+    }
+
+private fun postaviStatistiku() {
+    val stupac = findViewById<View>(R.id.barChart) as BarChart
+    val vrijednosti = ArrayList<BarEntry>()
+    vrijednosti.add(BarEntry(8f, 0))
+    vrijednosti.add(BarEntry(6f, 1))
+    vrijednosti.add(BarEntry(9f, 2))
+    vrijednosti.add(BarEntry(13f, 3))
+    //vrijednosti.add(BarEntry(11f, 4))
+
+
+
+    val podaciStupca = BarDataSet(vrijednosti, "Statistička raspodjela odabira vakcina")
+
+    val vakcine = ArrayList<String>()
+    vakcine.add("AstraZeneca")
+    vakcine.add("Pfizer")
+    vakcine.add("Sputnik V")
+    vakcine.add("Moderna")
+    // vakcine.add("Janssen")
+
+    val podaci = BarData(vakcine, podaciStupca)
+    stupac.data = podaci
+
+     stupac.setDescription("#vakcinišise")
+
+
+    podaciStupca.color = resources.getColor(R.color.teal_700)
+
+    stupac.animateY(2000)
+}
+
+
+}
+
