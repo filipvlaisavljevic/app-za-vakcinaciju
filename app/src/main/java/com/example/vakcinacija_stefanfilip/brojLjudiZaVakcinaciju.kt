86aab7abe4292f05b19b683d4b298994677278b4
Steffe Mech <stefkoRFlee@hotmail.com>
2021-04-22 13:48:10 +0200

    Zadnje 2 teze u izgledu, spojio sam ih na isti ekran, jer nema smisla da nam jednom ekranu bude samo brojac registrovanih. Takodjer, zamislio sam da se ne bismo puno pegali da mi imamo par svojih lokacija i vremena koje cemo dodjeliti ljudima kad se registuju i onda ih samo prikazati gdje su i kad dobili vakcinaciju...Brojac sam namjerno stavio malo veci da se vidi fino. Nek imamo vako za pocetak pa cemo mijenjati kasnije ako bude trebalo.

---
+++ app/src/main/java/com/example/vakcinacija_stefanfilip/brojLjudiZaVakcinaciju.kt
@@ -0,0 +1,11 @@
+package com.example.vakcinacija_stefanfilip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class brojLjudiZaVakcinaciju : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.broj_ljudi_za_vakcinaciju)
+    }
+}
