773e7b89c0a14e2c4057aa5ffaf8a8b2177fe617
Filip Vlaisavljevic <filipvlaisavljevic@outlook.com>
2021-05-09 19:05:21 +0200

    onFocusChange event za sitnice

--- app/src/main/java/com/example/vakcinacija_stefanfilip/PrijavaFragment.kt
+++ app/src/main/java/com/example/vakcinacija_stefanfilip/PrijavaFragment.kt
@@ -1,10 +1,10 @@
 package com.example.vakcinacija_stefanfilip
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
-import android.view.ViewGroup
+import android.view.View.OnTouchListener
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_prijava.*
 
@@ -35,22 +35,31 @@ class PrijavaFragment : Fragment(R.layout.fragment_prijava) {
                 drzava,
                 grad,
                 ulica,
-                datum)
+                datum
+            )
             if(!drzava.equals("Bosna i Hercegovina"))
                 action = PrijavaFragmentDirections.actionPrijavaFragmentToGreskaFragment()
 
             findNavController().navigate(action)
         }
+
+        prezimetext.setOnFocusChangeListener { v, hasFocus -> prezimetext.hint= ""  }
+        imetext.setOnFocusChangeListener { v, hasFocus ->  imetext.hint = ""}
+        datumtext.setOnFocusChangeListener { v, hasFocus ->  datumtext.hint = ""}
+        ulicatext.setOnFocusChangeListener{ v, hasFocus -> ulicatext.hint = ""}
+        gradtext.setOnFocusChangeListener { v, hasFocus -> gradtext.hint = ""}
+        datumtext.setOnFocusChangeListener { v, hasFocus ->  datumtext.hint = ""}
+        drzavatext.setOnFocusChangeListener { v, hasFocus ->  drzavatext.hint = ""}
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString("ime",ime)
-        outState.putString("prezime",prezime)
-        outState.putString("grad",grad)
-        outState.putString("ulica",ulica)
-        outState.putString("datum",datum)
-        outState.putString("drzava",drzava)
+        outState.putString("ime", ime)
+        outState.putString("prezime", prezime)
+        outState.putString("grad", grad)
+        outState.putString("ulica", ulica)
+        outState.putString("datum", datum)
+        outState.putString("drzava", drzava)
     }
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
