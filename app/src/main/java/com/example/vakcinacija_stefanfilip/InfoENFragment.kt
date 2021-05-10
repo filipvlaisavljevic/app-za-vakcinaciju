92d11ceeabb22ceb3f14aaf79845f914dfcd16f4
Filip Vlaisavljevic <filipvlaisavljevic@outlook.com>
2021-05-10 11:27:06 +0200

    Bottom nav bar, jos sidebar i gotovo.

---
+++ app/src/main/java/com/example/vakcinacija_stefanfilip/InfoENFragment.kt
@@ -0,0 +1,21 @@
+package com.example.vakcinacija_stefanfilip
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_info.*
+import kotlinx.android.synthetic.main.fragment_info_e_n.*
+
+class InfoENFragment : Fragment(R.layout.fragment_info_e_n) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bosanskitranslateinfoen.setOnClickListener{
+            val action = InfoENFragmentDirections.actionInfoENFragmentToInfoFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
